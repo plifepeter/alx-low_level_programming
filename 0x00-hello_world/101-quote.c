7ceb082ff9f8c5c4b382e79f09a7c049c7371981
Mbiakije Peter <pmbiakije@gmail.com>
2023-08-11 07:58:13 -0700

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Entry Poin
+ * Return: 1 (success)
+ */
+int main(void)
+{
+	write(stderr, "and that piece of art is useful\"
+			- Dora Korpar, 2015-10-19\n");
+	return (1);
+}
