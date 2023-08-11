1e1ee2790a0e5e65253eac3a87e109634369c252
Mbiakije Peter <pmbiakije@gmail.com>
2023-08-11 03:35:57 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio>
+/**
+ * main - A program that print new line with printf function
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
