7c9b627d75cdd768acf8f2aefc2a08155f4818b8
Mbiakije Peter <pmbiakije@gmail.com>
2023-08-13 10:24:56 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry Point
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	char i;
+
+	for (i = 'a'; i <= 'z'; i++)
+		putchar(i);
+
+	putchar('\n');
+
+	return (0);
+}
