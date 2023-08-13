4291f15833df8df2e28d3401e757e52f97a7b39d
Mbiakije Peter <pmbiakije@gmail.com>
2023-08-13 15:31:15 -0700

    9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - Entry Point
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		putchar((i % 10) + '0');
+	if (i == 9)
+		continue;
+
+	putchar(',');
+	putchar(' ');
+	}
+
+	putchar('\n');
+
+	return (0);
+}
