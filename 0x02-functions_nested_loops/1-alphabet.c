7a56dbeb417e775c6033e67eb7b8e8ef2d648dd9
Mbiakije Peter <pmbiakije@gmail.com>
2023-08-16 03:18:17 -0700

    1-alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * main - C programme that print Alphabet
+ * Return: 0 (success)
+ */
+void print_alphabet(void)
+{
+	char b;
+
+	for (b = 'a'; b <= 'z'; b++)
+	{
+		_putchar(b);
+	}
+
+	_putchar('\n');
+}
