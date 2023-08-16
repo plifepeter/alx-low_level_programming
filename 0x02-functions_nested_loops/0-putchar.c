16d8d81170a24a8f87f7d2bec56e5a530b12cd61
Mbiakije Peter <pmbiakije@gmail.com>
2023-08-16 02:43:54 -0700

    0-putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * main - C programme that Print _putchar with New Line
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+
+	return (0);
+}
