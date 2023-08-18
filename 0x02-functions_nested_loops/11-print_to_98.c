4568d125f4314155d27beae375d85180ba12a1e6
Mbiakije Peter <pmbiakije@gmail.com>
2023-08-18 09:25:21 -0700

    11-98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -8,30 +8,18 @@
 void print_to_98(int n)
 {
 	if (n < 98)
-		
 	{
 		while (n < 98)
-			
 		{
-			printf("%d, ", n);
-			n++;
+			printf("%d, ", n++);
 		}
-
 	}
-
 	if (n > 98)
-		
 	{
-		
 		while (n > 98)
-		
-		{	
-			printf("%d, ", n);
-			n--;
+		{
+			printf("%d, ", n--);
 		}
-
 	}
-
-	printf("98\n");
-
+	printf("98 \n");
 }
