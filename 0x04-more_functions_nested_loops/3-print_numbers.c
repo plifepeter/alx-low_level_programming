753c7c333d331c6b0e2cb6d3155f232ea76454b4
Mbiakije Peter <pmbiakije@gmail.com>
2023-08-22 06:11:06 -0700

    print numbers 0 to 9

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_numbers - print numbers 0 to 9
+ * Return: should return a value
+ */
+void print_numbers(void)
+{
+	int e;
+
+	for (e = 0; e <= 9; e++)
+
+	{
+		
+		_putchar(e + '0');
+	}
+
+	_putchar('\n');
+}
