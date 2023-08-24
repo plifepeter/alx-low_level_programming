20fb99778981fd07c7db24992270250288290de0
Mbiakije Peter <pmbiakije@gmail.com>
2023-08-24 07:02:28 -0700

    print line

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -6,19 +6,18 @@
  */
 void print_line(int n)
 {
-	int i = 0;
-
-	for (i = 0; i < n; i++)
-	{
-		_putchar('_');
-
-	}
-	{
-		_putchar('\n');
-	}
+	int i;
 
 	if (n <= 0)
 	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (i = 0; i < n; i++)
+		{
+			_putchar('_');
+		}
 		_putchar('\n');
 	}
 }
