33b5bb1a8f414ea37a2aa59c5d7db9b5429fb2f5
Mbiakije Peter <pmbiakije@gmail.com>
2023-08-25 09:05:36 -0700

    digonal

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -1,23 +1,32 @@
 #include "main.h"
 /**
- * print_line - function that draws line
+ * print_diagonal - function that draws diagonal
  * @n: find the variable n
  * Return: should return a value
  */
 void print_diagonal(int n)
 {
 	int i;
+	int e;
+
+	for (i = 0; i < n; i++)
+
+	{
+		if (n > 1)
+
+			for (e = 0; e < i; e++)
+
+		{
+			_putchar(' ');
+		}
+
+		_putchar('\\');
+		_putchar('\n');
+
+	}
 
 	if (n <= 0)
 	{
-		_putchar('\n');
-	}
-	else
-	{
-		for (i = 0; i < n; i++)
-		{
-			_putchar('\\');
-		}
 		_putchar('\n');
 	}
 }
