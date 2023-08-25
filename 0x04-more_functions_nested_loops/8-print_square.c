0e07d7a48e2936e494e6ec0096ed094fd6f59547
Mbiakije Peter <pmbiakije@gmail.com>
2023-08-25 09:44:43 -0700

    square

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * print_square - function that print square
+ * @size: look for the value of size
+ * Return: value should be successful
+ */
+void print_square(int size)
+{
+	int i;
+	int j;
+
+	if (size > 0)
+	{
+		for (i = 0; i < size; i++)
+		{
+			for (j = 0; j < size; j++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
+
