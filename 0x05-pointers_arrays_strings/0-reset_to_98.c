87d7ba77632f2b8a3231e8bcdec237ff10d71259
Mbiakije Peter <pmbiakije@gmail.com>
2023-08-29 06:14:52 -0700

    reset to 98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * reset_to_98 - function that take pointer 
+ * @n: look for the pointer of n variable
+ * Return: no value should return
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
